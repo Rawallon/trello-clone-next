f3e7fc144ac79193093dade8f36e4034855a40fa
rawallon <rawallon@gmail.com>
2021-05-08 15:59:29 -0300

    Created card component

---
+++ src/components/Card/index.js
@@ -0,0 +1,9 @@
+import styles from './card.module.css';
+
+export default function Card({ id, children }) {
+  return (
+    <div className={styles.cardBody} id={id} draggable="true">
+      {children}
+    </div>
+  );
+}
