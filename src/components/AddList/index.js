7397c0fe085eede94dbca7606b13991b845297dc
rawallon <rawallon@gmail.com>
2021-05-09 18:02:00 -0300

    Created AddList component

---
+++ src/components/AddList/index.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import styles from './addlist.module.css';
+
+function AddList({ createList }) {
+  const [showAddForm, setShowAddForm] = useState(false);
+  const [inputText, setInputText] = useState('');
+
+  function putListHandler() {
+    setShowAddForm(false);
+    setInputText('');
+    createList(inputText);
+  }
+
+  function onKeyPressHandler(event) {
+    if (event.charCode === 13) {
+      putListHandler();
+    }
+  }
+
+  return (
+    <>
+      <div
+        className={`
+    ${styles.listAdd}
+    ${showAddForm ? styles.hidden : ''}`}
+        onClick={() => setShowAddForm(!showAddForm)}>
+        Add new list
+      </div>
+      <div
+        className={`
+    ${styles.listAddForm}
+    ${showAddForm ? '' : styles.hidden}`}>
+        <input
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          onKeyPress={onKeyPressHandler}
+        />
+        <button onClick={putListHandler}>Add list</button>
+      </div>
+    </>
+  );
+}
+export default AddList;
