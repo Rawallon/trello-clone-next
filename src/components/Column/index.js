c38b5912b1828f57084a51faf1ae7502f0bd9632
rawallon <rawallon@gmail.com>
2021-05-09 11:48:11 -0300

    Created "Add card" input

--- src/components/Column/index.js
+++ src/components/Column/index.js
@@ -1,16 +1,16 @@
 import { useState } from 'react';
 import { Draggable, Droppable } from 'react-beautiful-dnd';
+import AutoResizableTextarea from '../AutoResizableTextarea';
 import styles from './column.module.css';
 
 export default function Column({ title = '', id, index, children }) {
-  const [isDraggingOver, setIsDraggingOver] = useState(false);
+  const [text, setText] = useState('');
+  const [showForm, setShowForm] = useState(false);
   return (
     <Draggable draggableId={id} index={index}>
       {(provided) => (
         <div
-          className={`${isDraggingOver ? styles.columnDraggingOver : ''} ${
-            styles.column
-          }`}
+          className={styles.column}
           ref={provided.innerRef}
           {...provided.draggableProps}
           id={id}>
@@ -33,6 +33,33 @@ export default function Column({ title = '', id, index, children }) {
               </div>
             )}
           </Droppable>
+          <footer
+            className={`${styles.footerColumn} ${
+              showForm ? styles.hidden : ''
+            }`}>
+            <button
+              className={styles.openForm}
+              onClick={() => setShowForm((prev) => !prev)}>
+              Add card
+            </button>
+          </footer>
+          <footer
+            className={`${styles.footerColumn}
+              ${showForm ? '' : styles.hidden}`}>
+            <div className={styles.TextareaWrapper}>
+              <AutoResizableTextarea
+                text={text}
+                setText={setText}
+                placeholder="Enter a title for this card..."
+                className={styles.AutoTextarea}
+              />
+            </div>
+            <button
+              className={styles.addCard}
+              onClick={() => setShowForm((prev) => !prev)}>
+              Add card
+            </button>
+          </footer>
         </div>
       )}
     </Draggable>
