dc57243d1956c6e774c6fd6f59b15d5fc639fea0
rawallon <rawallon@gmail.com>
2021-05-10 14:54:27 -0300

    Create Icons component

---
+++ src/components/Icons/index.js
@@ -0,0 +1,38 @@
+// Icons from https://iconic.app/
+
+export function CloseIcon() {
+  return (
+    <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        d="M17.25 6.75L6.75 17.25"></path>
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        d="M6.75 6.75L17.25 17.25"></path>
+    </svg>
+  );
+}
+export function PlusIcon() {
+  return (
+    <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        d="M12 5.75V18.25"></path>
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        d="M18.25 12L5.75 12"></path>
+    </svg>
+  );
+}
