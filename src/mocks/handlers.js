bc24391258ef1f4bd0e4e2bb783d340bf4872e6b
rawallon <rawallon@gmail.com>
2021-05-11 15:49:28 -0300

    Added data for Mock board fetch

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,6 +1,35 @@
 import { rest } from 'msw';
 export const handlers = [
-  rest.get('/test', (req, res, ctx) => {
-    return res(ctx.status(200));
+  rest.get('http://localhost:3000/board/1', (req, res, ctx) => {
+    return res(
+      ctx.json({
+        title: 'My board',
+        bgcolor: 'EFCB68',
+        lists: [
+          {
+            id: '1',
+            title: 'Doing',
+          },
+          {
+            id: '2',
+            title: 'Done',
+          },
+        ],
+        cards: [
+          {
+            id: '1-card',
+            name: 'Test Card',
+            description: '# Test',
+            list: '1',
+          },
+          {
+            id: '2-card',
+            name: 'Test 2',
+            description: '# Tested',
+            list: '1',
+          },
+        ],
+      }),
+    );
   }),
 ];
