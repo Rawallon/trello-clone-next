fd0af5c8ba362bfe2396c09069595ad4a8e23b46
rawallon <rawallon@gmail.com>
2021-05-12 11:23:47 -0300

    Implemented DB manipulations on MSW

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,35 +1,153 @@
 import { rest } from 'msw';
+import { db } from './db';
+
+db.board.create();
 export const handlers = [
-  rest.get('http://localhost:3000/board/1', (req, res, ctx) => {
-    return res(
-      ctx.json({
-        title: 'My board',
-        bgcolor: 'EFCB68',
-        lists: [
-          {
-            id: '1',
-            title: 'Doing',
-          },
-          {
-            id: '2',
-            title: 'Done',
-          },
-        ],
-        cards: [
-          {
-            id: '1-card',
-            name: 'Test Card',
-            description: '# Test',
-            list: '1',
-          },
-          {
-            id: '2-card',
-            name: 'Test 2',
-            description: '# Tested',
-            list: '1',
-          },
-        ],
-      }),
-    );
+  rest.post('/board/', (req, res, ctx) => {
+    const { title, bgcolor } = req.body;
+    const board = db.board.create({
+      id: Number(db.board.count) + 1,
+      title,
+      bgcolor,
+      cards: [],
+      lists: [],
+      permisionlist: ['1'],
+    });
+    return res(ctx.json(board));
+  }),
+  rest.get('/board/:id', (req, res, ctx) => {
+    const board = db.board.findFirst({
+      where: {
+        id: {
+          equals: req.params.id,
+        },
+      },
+    });
+    if (!board) {
+      return res(ctx.status(404));
+    }
+    return res(ctx.json(board));
+  }),
+  rest.get('http://localhost:3000/board/:id', (req, res, ctx) => {
+    const board = db.board.findFirst({
+      where: {
+        id: {
+          equals: req.params.id,
+        },
+      },
+    });
+    if (!board) {
+      return res(ctx.status(404));
+    }
+    return res(ctx.json(board));
+  }),
+  rest.post('/board/:id/card', (req, res, ctx) => {
+    const oldBoard = db.board.findFirst({
+      where: {
+        id: {
+          equals: req.params.id,
+        },
+      },
+    });
+    // On real back-end validation would be done
+    if (!oldBoard) {
+      return res(ctx.status(404));
+    }
+    const board = db.board.update({
+      where: {
+        id: {
+          equals: req.params.id,
+        },
+      },
+      data: {
+        cards: [...oldBoard.cards, req.body],
+      },
+    });
+    return res(ctx.json(board.cards));
+  }),
+  rest.patch('/board/:id/card', (req, res, ctx) => {
+    const oldBoard = db.board.findFirst({
+      where: {
+        id: {
+          equals: req.params.id,
+        },
+      },
+    });
+
+    const { cardId, toId, insertIndex } = req.body;
+
+    const cIndex = oldBoard.cards.findIndex((c) => c.id === cardId);
+    const newCards = [...oldBoard.cards];
+    const cCard = newCards.splice(cIndex, 1)[0];
+    cCard.list = toId;
+    newCards.splice(insertIndex, 0, cCard);
+
+    const board = db.board.update({
+      where: {
+        id: {
+          equals: req.params.id,
+        },
+      },
+      data: {
+        cards: newCards,
+      },
+    });
+    return res(ctx.json(board.cards));
+  }),
+  rest.put('/board/:id/card', (req, res, ctx) => {
+    const oldBoard = db.board.findFirst({
+      where: {
+        id: {
+          equals: req.params.id,
+        },
+      },
+    });
+
+    const { newData } = req.body;
+
+    const cIndex = oldBoard.cards.findIndex((c) => c.id === newData.id);
+    const newCards = [...oldBoard.cards];
+    newCards.splice(cIndex, 1)[0];
+    const cCard = newData;
+    newCards.splice(cIndex, 0, cCard);
+
+    const board = db.board.update({
+      where: {
+        id: {
+          equals: req.params.id,
+        },
+      },
+      data: {
+        cards: newCards,
+      },
+    });
+    return res(ctx.json(board.cards));
+  }),
+  rest.post('/board/:id/list', (req, res, ctx) => {
+    const oldBoard = db.board.findFirst({
+      where: {
+        id: {
+          equals: req.params.id,
+        },
+      },
+    });
+
+    const { listId, insertIndex } = req.body;
+
+    const cIndex = oldBoard.lists.findIndex((c) => c.id === listId);
+    const newList = [...oldBoard.lists];
+    newList.splice(insertIndex, 0, newList.splice(cIndex, 1)[0]);
+
+    const board = db.board.update({
+      where: {
+        id: {
+          equals: req.params.id,
+        },
+      },
+      data: {
+        lists: newList,
+      },
+    });
+    return res(ctx.json(board.lists));
   }),
 ];
