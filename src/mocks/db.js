6d74b42d5b9ce6e10cb867018e4a92b6db00bb84
rawallon <rawallon@gmail.com>
2021-05-12 11:21:52 -0300

    Created Mock db with MSW Data

---
+++ src/mocks/db.js
@@ -0,0 +1,33 @@
+import { factory, primaryKey } from '@mswjs/data';
+
+export const db = factory({
+  board: {
+    id: primaryKey(() => '1'),
+    title: () => 'My board',
+    bgcolor: () => 'EFCB68',
+    lists: () => [
+      {
+        id: '1',
+        title: 'Doing',
+      },
+      {
+        id: '2',
+        title: 'Done',
+      },
+    ],
+    cards: () => [
+      {
+        id: '1-card',
+        name: 'Test Card 1',
+        description: '# Test',
+        list: '1',
+      },
+      {
+        id: '2-card',
+        name: 'Test Card 2',
+        description: '# Tested',
+        list: '1',
+      },
+    ],
+  },
+});
