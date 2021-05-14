96b33ec1cee2ab9dbc1e55c2c8d39434be47c806
rawallon <rawallon@gmail.com>
2021-05-14 18:28:42 -0300

    Created Card test

---
+++ src/components/BoardPage/Card/Card.spec.js
@@ -0,0 +1,16 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Card from '.';
+
+const propMock = {
+  id: 'test',
+  children: <div>Test me</div>,
+};
+let card;
+beforeEach(() => {
+  card = render(<Card {...propMock} />);
+});
+
+it('should render with child properly', () => {
+  expect(card.getByText('Test me')).toBeInTheDocument();
+});
