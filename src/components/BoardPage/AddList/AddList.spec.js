fa9076f45f6044b7c9ab6894a0e9dad32ddd64e8
rawallon <rawallon@gmail.com>
2021-05-14 18:23:39 -0300

    Created AddList test

---
+++ src/components/BoardPage/AddList/AddList.spec.js
@@ -0,0 +1,30 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import AddList from '.';
+
+var addList;
+var createListMock = jest.fn();
+beforeEach(() => {
+  addList = render(<AddList createList={createListMock} />);
+});
+
+it('should render properly', () => {
+  expect(addList.getByText('Add list')).toBeInTheDocument();
+});
+
+it('Input shoud initially hidden', () => {
+  expect(addList.getByText('Add new list')).toBeInTheDocument();
+  expect(addList.getByRole('textbox', { hidden: true })).toBeInTheDocument();
+});
+
+it('Input shoud appear on button click', () => {
+  expect(addList.getByText('Add new list')).toBeInTheDocument();
+  fireEvent.click(addList.getByText('Add new list'));
+  expect(addList.getByRole('textbox', { hidden: false })).toBeInTheDocument();
+});
+
+it('should call createList on button click', () => {
+  const addListBtn = addList.getByText('Add list');
+  fireEvent.click(addListBtn);
+  expect(createListMock).toHaveBeenCalled();
+});
