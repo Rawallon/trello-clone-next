3f44cb1ba18e0f7a815a5158e7f7295f68dcaafd
rawallon <rawallon@gmail.com>
2021-05-16 17:10:05 -0300

    Covered missing statemente on AddList

--- src/components/BoardPage/AddList/AddList.spec.js
+++ src/components/BoardPage/AddList/AddList.spec.js
@@ -23,6 +23,26 @@ it('Input shoud appear on button click', () => {
   expect(addList.getByRole('textbox', { hidden: false })).toBeInTheDocument();
 });
 
+it('onChange should update text input', () => {
+  expect(addList.getByText('Add new list')).toBeInTheDocument();
+  fireEvent.click(addList.getByText('Add new list'));
+  const textInput = addList.getByRole('textbox', { hidden: false });
+  expect(textInput).toBeInTheDocument();
+  fireEvent.change(textInput, { target: { value: 'test' } });
+});
+
+it('pressing enter on input should call createListMock', () => {
+  expect(addList.getByText('Add new list')).toBeInTheDocument();
+  fireEvent.click(addList.getByText('Add new list'));
+  expect(addList.getByRole('textbox', { hidden: false })).toBeInTheDocument();
+  fireEvent.keyPress(addList.getByRole('textbox', { hidden: false }), {
+    key: 'Enter',
+    code: 13,
+    charCode: 13,
+  });
+  expect(createListMock).toHaveBeenCalled();
+});
+
 it('should call createList on button click', () => {
   const addListBtn = addList.getByText('Add list');
   fireEvent.click(addListBtn);
