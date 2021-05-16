394ecc8815b228dbc7393db75eeea2eb7adee3a5
rawallon <rawallon@gmail.com>
2021-05-16 17:07:56 -0300

    Fixed folder to get coverage from

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   bail: 1,
   clearMocks: true,
   collectCoverage: true,
-  collectCoverageFrom: ['components/**/*.js', 'pages/**/*.js'],
+  collectCoverageFrom: ['./src/components/**/*.js'],
   coverageReporters: ['lcov', 'text'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
