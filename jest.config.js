515ac301542b781c8a2761f74bb953d410f1a2c3
rawallon <rawallon@gmail.com>
2021-05-18 12:27:05 -0300

    Added context folder to coverage

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   bail: 1,
   clearMocks: true,
   collectCoverage: true,
-  collectCoverageFrom: ['./src/components/**/*.js'],
+  collectCoverageFrom: ['./src/components/**/*.js','./src/context/*.js'],
   coverageReporters: ['lcov', 'text'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
