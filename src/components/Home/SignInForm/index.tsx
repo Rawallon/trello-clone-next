a9cbf0431f8ff0bfe751c29bcecfb914bb265514
rawallon <rawallon@gmail.com>
2021-07-16 11:19:00 -0300

    Added logo to signIn form

--- src/components/Home/SignInForm/index.tsx
+++ src/components/Home/SignInForm/index.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
+import Image from 'next/image';
+
 import SignInButton from '../SignInButton';
 import style from './SignInForm.module.css';
 
+import logo from '../assets/logo.svg';
 import google from '../assets/google.svg';
 import github from '../assets/github.svg';
 
 export function SignInForm({ signIn }) {
   return (
-    <div className={style.signInForm}>
-      <h1>Sign In</h1>
-      <SignInButton
-        signIn={() => signIn('google')}
-        img={google}
-        text="Sign in with Google"
-      />
-      <SignInButton
-        signIn={() => signIn('github')}
-        img={github}
-        text="Sign in with Github"
-      />
-    </div>
+    <>
+      <div className={style.container}>
+        <div className={style.signInForm}>
+          <div className={style.header}>
+            <Image src={logo.src} layout="intrinsic" width={170} height={42} />
+          </div>
+          <div className={style.separator}>Sign In</div>
+          <SignInButton
+            signIn={() => signIn('google')}
+            img={google}
+            text="Sign in with Google"
+          />
+          <SignInButton
+            signIn={() => signIn('github')}
+            img={github}
+            text="Sign in with Github"
+          />
+        </div>
+      </div>
+    </>
   );
 }
